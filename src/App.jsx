a58b043f2fed7470bdec84f48a0a03a1e09f17e8
Shekinah Ogo-Ayorinde <ogoayorindeshekinah@gmail.com>
2026-01-07 21:52:29 +0100

    Revert 'update to configurator'

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import EnhancedConfigurator from "./EnhancedConfigurator";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
+import Configurator from "./Configurator";
 
 function App() {
-  return <EnhancedConfigurator />;
+  return <Configurator />;
 }
 
 export default App;
