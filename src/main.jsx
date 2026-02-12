fdfd0054ae0aff56af82181fde752893a4e56cae
Ogo-Ayorinde Shekinah <68741518+Oluwafemi-web@users.noreply.github.com>
2026-02-12 12:45:35 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+//trigger redeploy
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
